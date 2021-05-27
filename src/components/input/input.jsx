13bb9a40cf500a0dab021d8f5222ccfebb079ebb
Eyder Herrera <alexxherrerac@gmail.com>
2021-05-26 19:39:17 -0500

    funcion para el API

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -6,6 +6,27 @@ import './input-styles.scss'
 const Input = () => {
     const { setState } = useContext(Context)
 
+    const getData = async (ipAddress) => {
+        const apiUrl = `https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=at_9Tpp85nngTjzfoaDORe0x2XJHjMh7&ipAddress=${ipAddress}`
+        const response = await axios.get(apiUrl)
+        const { ip, isp, location } = await response.data
+        setState({
+            ip: ip,
+            city: location.city,
+            region: location.region,
+            postalCode: location.postalCode,
+            lat: location.lat,
+            lng: location.lng,
+            timezone: location.timezone,
+            isp: isp
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getData(e.target.firstElementChild.value);
+    }
+
     return (
         <div className="input-wrap">
             <form onSubmit={handleSubmit}>
